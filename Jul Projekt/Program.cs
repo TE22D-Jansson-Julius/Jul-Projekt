4ceb5b081323ae3ca344757aa63aa0c1734120ea
Bullen <julius.jansson@elev.ga.ntig.se>
2024-01-31 12:38:36 +0100

    joijoij

--- Jul Projekt/Program.cs
+++ Jul Projekt/Program.cs
@@ -1,12 +1,22 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
 Raylib.InitWindow(800, 600, "very hard game");
 
+Vector2 movement = new Vector2(0.1f, 0.1f);
 
 Rectangle wall = new Rectangle(64, 0, 16, 200);
+Rectangle character = new Rectangle(20, 300, 40, 40);
 
+float speed = 3;
 
 string scene = "start";
+
+
+
+//--------------------------------------------------------------------------------------------------------
+//-------------------------------------------Graphics-----------------------------------------------------
+//--------------------------------------------------------------------------------------------------------
 
 
 while (!Raylib.WindowShouldClose())
@@ -22,31 +32,31 @@ while (!Raylib.WindowShouldClose())
     else if (scene == "game")
     {
 
+        movement = Vector2.Zero;
 
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        {
+            movement.X = -1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        {
+            movement.X = 1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+        {
+            movement.Y = -1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+        {
+            movement.Y = 1;
+        }
+        if (movement.Length() > 0)
+        {
+            movement = Vector2.Normalize(movement) * speed;
+        }
 
-        //  if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
-        //     {
-        //       movement.X = -1;
-        //     }
-        //     else if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
-        //     {
-        //       movement.X = 1;
-        //     }
-        //     if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
-        //     {
-        //       movement.Y = -1;
-        //     }
-        //     else if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
-        //     {
-        //       movement.Y = 1;
-        //     }
-        //     if (movement.Length() > 0)
-        //     {
-        //       movement = Vector2.Normalize(movement) * speed;
-        //     }
-
-        //     characterRect.x += movement.X;
-        //     characterRect.y += movement.Y;
+        character.X += movement.X;
+        character.Y += movement.Y;
 
 
     }
@@ -68,13 +78,16 @@ while (!Raylib.WindowShouldClose())
         Raylib.DrawText("Welcome to the worlds hardest game", 50, 250, 35, Color.BLACK);
         Raylib.DrawText("Press Space to play", 75, 285, 35, Color.BLACK);
     }
-    else if (scene == "game"){
+    else if (scene == "game")
+    {
+        Raylib.ClearBackground(Color.RED);
+        Raylib.DrawText("W,A,S,D To Move", 10, 10, 40, Color.BLACK);
 
+        Raylib.DrawRectangleRec(character, Color.BLUE);
 
-        
     }
 
-        Raylib.EndDrawing();
+    Raylib.EndDrawing();
 
 
 
